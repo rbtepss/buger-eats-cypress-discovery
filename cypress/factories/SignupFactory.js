c22926dcab5269c565152c2c649a7ec7a516efc2
rbtepss <robertbernardoc.dearaujo@gmail.com>
2022-01-29 17:36:32 -0300

    Primeiro commit

---
+++ cypress/factories/SignupFactory.js
@@ -0,0 +1,33 @@
+var faker = require ('faker')
+
+var cpf = require('gerador-validador-cpf')
+
+export default {
+
+    deliver: function() {
+
+        var firstName = faker.name.firstName()
+        var lastName = faker.name.lastName()
+
+        var data = {
+            name: `${firstName} ${lastName}`,
+            cpf: cpf.generate(),
+            email: faker.internet.email(firstName),
+            whatsapp: '81999999999',
+            address: {
+                postalCode: '51240810',
+                street: 'Rua Pederneiras',
+                number: '116',
+                details: 'Casa',
+                district: 'Ibura',
+                city_State: 'Recife/PE'
+            },
+            delivery_method: 'Moto',
+            cnh: 'cnh-digital.jpg'
+        }  
+
+        return data
+
+    }
+
+}
